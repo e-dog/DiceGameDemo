2cf61d53fda76c3896eefac50b883f26d647f5aa
e-dog <e-dog@users.noreply.github.com>
2024-07-02 14:25:50 +0300

    Azure hosting.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using DiceGame.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSignalR().AddAzureSignalR(Environment.GetEnvironmentVariable("AZURE_SIGNALR_CONNECTIONSTRING"));
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
